bb986f8a58c30e205ce28e16bd3ed8c43b8344ca
Nolan Kovacik <noltron000@gmail.com>
2018-11-27 16:06:17 -0800

    changing order of code to be consistant with tutorial

--- docs/ui.js
+++ docs/ui.js
@@ -23,6 +23,83 @@ OregonH.UI.refreshStats = function () {
 	document.getElementById('caravan').style.left = (380 * this.caravan.distance / OregonH.FINAL_DISTANCE) + 'px';
 };
 
+//show shop
+OregonH.UI.showShop = function (products) {
+
+	//get shop area
+	var shopDiv = document.getElementById('shop');
+	shopDiv.classList.remove('hidden');
+
+	//init the shop just once
+	if (!this.shopInitiated) {
+
+		//event delegation
+		shopDiv.addEventListener('click', function (e) {
+			//what was clicked
+			var target = e.target || e.src;
+
+			//exit button
+			if (target.tagName == 'BUTTON') {
+				//resume journey
+				shopDiv.classList.add('hidden');
+				OregonH.UI.game.resumeJourney();
+			}
+			else if (target.tagName == 'DIV' && target.className.match(/product/)) {
+
+				console.log('buying')
+
+				var bought = OregonH.UI.buyProduct({
+					item: target.getAttribute('data-item'),
+					qty: target.getAttribute('data-qty'),
+					price: target.getAttribute('data-price')
+				});
+
+				if (bought) target.html = '';
+			}
+		});
+
+		this.shopInitiated = true;
+	}
+
+	//clear existing content
+	var prodsDiv = document.getElementById('prods');
+	prodsDiv.innerHTML = '';
+
+	//show products
+	var product;
+	for (var i = 0; i < products.length; i++) {
+		product = products[i];
+		prodsDiv.innerHTML += '<div class="product" data-qty="' + product.qty + '" data-item="' + product.item + '" data-price="' + product.price + '">' + product.qty + ' ' + product.item + ' - $' + product.price + '</div>';
+	}
+
+	//setup click event
+	//document.getElementsByClassName('product').addEventListener(OregonH.UI.buyProduct);
+};
+
+//buy product
+OregonH.UI.buyProduct = function (product) {
+	//check we can afford it
+	if (product.price > OregonH.UI.caravan.money) {
+		OregonH.UI.notify('Not enough money', 'negative');
+		return false;
+	}
+
+	OregonH.UI.caravan.money -= product.price;
+
+	OregonH.UI.caravan[product.item] += +product.qty;
+
+	OregonH.UI.notify('Bought ' + product.qty + ' x ' + product.item, 'positive');
+
+	//update weight
+	OregonH.UI.caravan.updateWeight();
+
+	//update visuals
+	OregonH.UI.refreshStats();
+
+	return true;
+
+};
+
 //show attack
 OregonH.UI.showAttack = function (firepower, gold) {
 	var attackDiv = document.getElementById('attack');
@@ -97,72 +174,4 @@ OregonH.UI.runaway = function () {
 	document.getElementById('attack').classList.add('hidden');
 	this.game.resumeJourney();
 
-};
-
-//show shop
-OregonH.UI.showShop = function (products) {
-
-	//get shop area
-	var shopDiv = document.getElementById('shop');
-	shopDiv.classList.remove('hidden');
-
-	//init the shop just once
-	if (!this.shopInitiated) {
-
-		//event delegation
-		shopDiv.addEventListener('click', function (e) {
-			//what was clicked
-			var target = e.target || e.src;
-
-			//exit button
-			if (target.tagName == 'BUTTON') {
-				//resume journey
-				shopDiv.classList.add('hidden');
-				OregonH.UI.game.resumeJourney();
-			}
-			else if (target.tagName == 'DIV' && target.className.match(/product/)) {
-
-				OregonH.UI.buyProduct({
-					item: target.getAttribute('data-item'),
-					qty: target.getAttribute('data-qty'),
-					price: target.getAttribute('data-price')
-				});
-
-			}
-		});
-
-		this.shopInitiated = true;
-	}
-
-	//clear existing content
-	var prodsDiv = document.getElementById('prods');
-	prodsDiv.innerHTML = '';
-
-	//show products
-	var product;
-	for (var i = 0; i < products.length; i++) {
-		product = products[i];
-		prodsDiv.innerHTML += '<div class="product" data-qty="' + product.qty + '" data-item="' + product.item + '" data-price="' + product.price + '">' + product.qty + ' ' + product.item + ' - $' + product.price + '</div>';
-	}
-};
-
-//buy product
-OregonH.UI.buyProduct = function (product) {
-	//check we can afford it
-	if (product.price > OregonH.UI.caravan.money) {
-		OregonH.UI.notify('Not enough money', 'negative');
-		return false;
-	}
-
-	OregonH.UI.caravan.money -= product.price;
-
-	OregonH.UI.caravan[product.item] += +product.qty;
-
-	OregonH.UI.notify('Bought ' + product.qty + ' x ' + product.item, 'positive');
-
-	//update weight
-	OregonH.UI.caravan.updateWeight();
-
-	//update visuals
-	OregonH.UI.refreshStats();
 };
